16172f91c2a57313288243839a02806b7f196fb3
black-calculus <52509873+black-calculus@users.noreply.github.com>
2021-08-01 16:39:44 -0700

    Add ENABLE_DOUBLE_TRACTION
    Added a flag to determine when to double traction

--- src/lib.rs
+++ src/lib.rs
@@ -251,6 +251,7 @@ pub mod consts {
         pub const PERFECT_WAVEDASH:             i32 = 0x31;
         pub const SHOULD_WAVELAND:              i32 = 0x32;
         pub const DISABLE_SHORTHOP:             i32 = 0x33;
+        pub const ENABLE_DOUBLE_TRACTION:       i32 = 0x34;
 
         // float consts
         pub const ECB_Y_OFFSET:                i32 = 0x0;
