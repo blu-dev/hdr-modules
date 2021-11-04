f8e0acd2f235f052b5888e90ad1b39664e87656d
techyCoder81 <42820193+techyCoder81@users.noreply.github.com>
2021-11-03 20:05:10 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -297,7 +297,8 @@ pub mod consts {
         pub const ESCAPE_AIR_SLIDE_SPEED_Y:    i32 = 0x14;
         pub const CURR_DASH_SPEED:             i32 = 0x15;
         pub const MOONWALK_SPEED:              i32 = 0x16;
-        pub const ORIGINAL_LANDING_LAG:         i32 = 0x17;
+        pub const ORIGINAL_LANDING_LAG:        i32 = 0x17;
+        pub const LAST_CLEARED_FRAME:          i32 = 0x18;
 
         // generic consts
         pub const HITFALL_BUFFER:              i32 = 0x0; // VecDeque<bool>
